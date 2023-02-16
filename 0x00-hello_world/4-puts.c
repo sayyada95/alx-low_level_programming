ae50063df06086832effffb16f7232b6629e701c
sayyada95 <aishamuhammad082@gmail.com>
2023-02-16 12:19:02 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 /**
- * main - Entry
- *  Return: Always 0
+ * main - Entry now
+ *  Return: Always 0 (Success)
  */
+
 int main(void)
 {
 	puts("\"Programming is like building a multi lingual puzzle");
 	return (0);
-
+}
