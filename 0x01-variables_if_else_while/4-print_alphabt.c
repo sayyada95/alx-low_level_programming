66e9b0c148048df7033675a49d4e6a328e940c01
sayyada95 <aishamuhammad082@gmail.com>
2023-02-17 17:55:34 -0800

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0
+ */
+int main(void)
+{
+	int ls;
+
+	for (ls = 'a'; ls <= 'z'; ch++)
+	{
+	if (ls != 'e' && ls != 'q')
+		putchar(ls);
+	}
+	else
+	{
+	}
+	}
+	putchar('\n');
+	return (0);
+}
