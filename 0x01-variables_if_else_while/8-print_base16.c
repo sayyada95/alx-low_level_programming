6c562ed715aee847490013a62de47e7c5c83020e
sayyada95 <aishamuhammad082@gmail.com>
2023-02-19 12:50:22 -0800

    8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -3,17 +3,22 @@
  * main - prints the numbers in base 16
  * Return: Always 0
  */
-int miain(void)
+int main(void)
 {
-	int num;
-	char la;
-
-	for (num = 0; num < 10; num++)
-		putchar((num % 10) + '0');
-
-	for (la = 'a'; la <= 'f'; la++)
-		putchar(la);
-
+	char c;
+	int d;
+c = 'a';
+	d = 0;
+	while
+		(d < 10) {
+			putchar(d + '0');
+			d++;
+		}
+	while
+		(c <= 'f') {
+			putchar(c);
+			c++
+		}
 	putchar('\n');
 	return (0);
 }
