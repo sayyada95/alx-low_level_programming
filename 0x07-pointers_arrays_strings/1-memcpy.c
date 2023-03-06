0f4a31374afa77cf1f1aa0fe8c89e34744856925
sayyada95 <aishamuhammad082@gmail.com>
2023-03-06 13:05:09 -0800

    2-strchr.c

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -7,3 +7,23 @@
  *
  *Return: copied memory with n byted changed
  */
+void simple_print_buffer(char *buffer, unsigned int size)
+{
+    unsigned int i;
+
+    i = 0;
+    while (i < size)
+    {
+        if (i % 10)
+        {
+            printf(" ");
+        }
+        if (!(i % 10) && i)
+        {
+            printf("\n");
+        }
+        printf("0x%02x", buffer[i]);
+        i++;
+    }
+    printf("\n");
+}
